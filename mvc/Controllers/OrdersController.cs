e736418a70a5c9b2e9571e702dc648758c23c78f
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2019-07-29 17:40:04 +0300

    Update OrdersController.cs

--- mvc/Controllers/OrdersController.cs
+++ mvc/Controllers/OrdersController.cs
@@ -1,19 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using DataGridMasterDetailAccessMVC.Models;
 using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
+using System.Net.Http;
+using System.Web.Http;
+using System.Linq;
 
 namespace DataGridMasterDetailAccessMVC.Controllers {
     public class DataGridMasterDetailViewController : ApiController {
-
         [HttpGet]
         public HttpResponseMessage Tasks(DataSourceLoadOptions loadOptions) {
-            return Request.CreateResponse(DataSourceLoader.Load(SampleData.DataGridEmployees.Where(e => e.ID <= 9), loadOptions));
+            return Request.CreateResponse(DataSourceLoader.Load(SampleData.DataGridEmployees.Where(e => e.ID <= 3), loadOptions));
         }
 
         [HttpGet]
@@ -21,6 +16,5 @@ namespace DataGridMasterDetailAccessMVC.Controllers {
             var tasks = SampleData.DataGridEmployees.Where(e => e.ID == id).Select(e => e.Tasks).FirstOrDefault();
             return Request.CreateResponse(DataSourceLoader.Load(tasks, loadOptions));
         }
-
     }
 }
