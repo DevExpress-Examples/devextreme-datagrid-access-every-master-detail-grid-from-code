a4eee299d0516cf098ba35aee5bcf57a6d8a7c35
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2019-07-30 18:33:12 +0300

    Update main.js

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import 'devextreme/dist/css/dx.common.css';
-import 'devextreme/dist/css/dx.light.compact.css';
+import 'devextreme/dist/css/dx.light.css';
 
 Vue.config.productionTip = false
 
