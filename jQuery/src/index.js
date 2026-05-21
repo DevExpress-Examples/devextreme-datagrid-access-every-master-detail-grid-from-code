f1d39ddb3957152f6a73f602aa3f78b4f01289c4
Paul Jeremy Manuel <peejheymanuel@gmail.com>
2026-05-21 18:03:30 +0800

    jQuery sample fix - initial commit

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,42 @@
+import { employees, tasks } from "./data.js";
+
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
-    },
+  $("#gridContainer").dxDataGrid({
+    dataSource: employees,
+    keyExpr: "ID",
+    showBorders: true,
+    columns: ["FirstName", "LastName", "Position", "State", "BirthDate"],
+    masterDetail: {
+      enabled: true,
+      template: function (container, options) {
+        var currentEmployeeData = options.data;
+        var detailGridId = `detailGrid${options.key}`;
+
+        $("<div>").dxButton({
+          text: "Focus row",
+          onClick: function (e) {
+            var detailGrid = $(`#${detailGridId}`).dxDataGrid("instance");
+            detailGrid.option("focusedRowIndex", 0);
+          }
+        }).appendTo(container);
+
+        $("<div>")
+          .dxDataGrid({
+            elementAttr: {
+              id: detailGridId
+            },
+            focusedRowEnabled: true,
+            showBorders: true,
+            columns: ["Subject", "StartDate", "DueDate", "Priority"],
+            dataSource: new DevExpress.data.DataSource({
+              store: new DevExpress.data.ArrayStore({
+                key: "ID",
+                data: tasks
+              }),
+              filter: ["EmployeeID", "=", options.key]
+            })
+          }).appendTo(container);
+      }
+    }
   });
 });
