1d9e1a075bbf260a80141c2be6b7c012fb59bbaa
Paul Jeremy Manuel <peejheymanuel@gmail.com>
2026-05-21 17:31:02 +0800

    Remove legacy lowercase framework folders

--- angular/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideZoneChangeDetection } from '@angular/core';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideZoneChangeDetection()],
-}).catch((err) => console.error(err));
