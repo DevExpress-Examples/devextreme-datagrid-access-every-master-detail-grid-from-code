faf405b801a4d9c7a79e062bd49c24b41daf9b9e
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 11:19:42 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { DxButtonModule } from 'devextreme-angular/ui/button';
 import { ClickEvent } from 'devextreme/ui/button';
 
@@ -6,6 +6,7 @@ import { ClickEvent } from 'devextreme/ui/button';
     selector: 'app-root',
     imports: [DxButtonModule],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
